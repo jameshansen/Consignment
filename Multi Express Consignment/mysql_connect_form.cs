4a052779035aeffc378bcdff7b3cb86834003e3f
James Hansen <1550976+jameshansen@users.noreply.github.com>
2011-01-12 13:26:26 -0800

    created mysql connection form and the global database connection classes

---
+++ Multi Express Consignment/mysql_connect_form.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+
+using System.Data.OleDb;
+using System.Xml.Serialization;
+
+using System.IO;
+using System.Diagnostics;
+using Ini;
+
+
+namespace Multi_Express_Consignment
+{
+    public partial class MysqlConnectForm : Form
+    {
+
+        private Form1 m_parent;
+        private int isConnected = 0;
+        private int countdown;
+
+        public MysqlConnectForm(Form1 frm1)
+        {
+            InitializeComponent();
+            m_parent = frm1;
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void MysqlConnectForm_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Check if Connected
+            if (isConnected == 1)
+            {
+                /* Now Connected, Close Self and Notify Form 1 */
+                timer1.Enabled = false;
+                this.Close(); // Close Self
+                m_parent.onConnect(); // Trigger on Connect Routine                              
+                this.Dispose();
+            }
+
+            // 2 = error
+            if (isConnected == 2)
+            {
+                // Countdown
+                countdown = countdown - 1;               
+                label1.Text = "Connection Failure... Retrying in " + Convert.ToString(countdown) + " seconds...";
+
+                if (countdown == 0)
+                {
+                    isConnected = 0;
+                    label1.Text = "Establishing Connection to MySQL Database...";
+                    backgroundWorker1.RunWorkerAsync(); // Retry Connection
+                }
+            }
+
+            
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // MySQL Connect
+            /* Load Ini File Settings */
+            string mysqlServer = iniglobal.ini.IniReadValue("mysql", "server");
+            string mysqlDatabase = iniglobal.ini.IniReadValue("mysql", "database");
+            string mysqlUser = iniglobal.ini.IniReadValue("mysql", "user");
+            string mysqlPassword = iniglobal.ini.IniReadValue("mysql", "password");
+
+            /* Connect to MySQL database */
+            string strProvider = "Data Source=" + mysqlServer + ";Database=" + mysqlDatabase + ";User ID=" + mysqlUser + ";Password=" + mysqlPassword;
+            try
+            {
+                mysqlglobal.mysqlCon = new MySqlConnection(strProvider);
+                mysqlglobal.mysqlCon.Open();
+                isConnected = 1;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                label1.Text = "Connection Failure... Retrying in 5 seconds...";
+                countdown = 5; // Retry in five seconds
+                isConnected = 2;
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MysqlConnectForm_Shown(object sender, EventArgs e)
+        {
+            // Center in Parent Window
+            this.Top = m_parent.Top + (m_parent.Height / 2) - (this.Height / 2);
+            this.Left = m_parent.Left + (m_parent.Width / 2) - (this.Width / 2);
+        }
+    }
+}
---
+++ Multi Express Consignment/mysqlglobal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+
+using System.Data.OleDb;
+using System.IO;
+
+using Ini;
+
+namespace Multi_Express_Consignment
+{
+    public class mysqlglobal
+    {
+        public static MySqlConnection mysqlCon = null;
+    }
+
+    public class dbfglobal
+    {
+        public static OleDbConnection dbfCon = null;
+    }
+
+    public class logglobal
+    {
+        public static TextWriter logFile = null;
+    }
+
+    public class iniglobal
+    {
+        public static IniFile ini = null;
+    }
+}
