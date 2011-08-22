c2113e333e4810bc243ba29969294b29e46346fa
James Hansen <1550976+jameshansen@users.noreply.github.com>
2011-08-22 15:16:16 -0700

    cash log report and update the print-reports UI and payment/daily sales reports

--- Multi Express Consignment/print_reports.Designer.cs
+++ Multi Express Consignment/print_reports.Designer.cs
@@ -35,6 +35,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -93,12 +101,97 @@
             this.label3.Size = new System.Drawing.Size(137, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Net Payment-In/Out Report";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(12, 248);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(41, 41);
+            this.button4.TabIndex = 6;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Daily Sales Report";
+            // 
+            // button5
+            // 
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(12, 306);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(41, 41);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(70, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Order Detail Report";
+            // 
+            // button6
+            // 
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(12, 364);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(41, 41);
+            this.button6.TabIndex = 10;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(70, 378);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Consolidated Consignor Report";
+            // 
+            // button7
+            // 
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(12, 189);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(41, 41);
+            this.button7.TabIndex = 12;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(74, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Cash Log Report";
             // 
             // print_reports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(306, 194);
+            this.ClientSize = new System.Drawing.Size(306, 423);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -122,5 +215,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label7;
     }
 }
--- Multi Express Consignment/print_reports_options.cs
+++ Multi Express Consignment/print_reports_options.cs
@@ -21,25 +21,116 @@ namespace Multi_Express_Consignment
             report_name = reportName;
         }
 
+        private void print_reports_options_Load(object sender, EventArgs e)
+        {
+            this.Width = 384;
+
+            if (report_name == "Consolidated Consignor Report")
+            {
+                tabControl1.Visible = false;
+                groupBox1.Left = tabControl1.Left;
+            }
+
+            /* If Report Name is different to Daily Sales Report, Hide Extra Options*/
+            if (report_name != "Daily Sales Report" && report_name != "Order Detail Report")
+            {
+                button1.Top = 202;
+                button2.Top = 202;
+                this.Height = 262;
+                dailysalesreport_options.Visible = false;
+            }
+
+            if (report_name == "Daily Sales Report")
+            {
+                report_detailed.Visible = false;
+                report_summary.Visible = false;
+            }
+
+            /* Populate Month Dropdown */
+
+            // Get first entry from database
+            string firstDateDB = mysqlglobal.executeScalarQuery("SELECT `date_order` FROM `cstorder` ORDER BY `date_order` ASC LIMIT 1", this).ToString();
+
+            DateTime firstDate = mysqlglobal.ConvertFromUnixTimestamp(Convert.ToDouble(firstDateDB));
+
+            DateTime curDate = firstDate;
+            int a = 0;
+            int final = Convert.ToInt32(DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString());
+            while (a < final)
+            {
+                a = Convert.ToInt32(curDate.Year.ToString() + curDate.Month.ToString());
+                sMonthBox.Items.Add(curDate.ToString("MMMM yyyy"));
+                //MessageBox.Show(@"a: " + a + ". final: " + final + ". Datestring: " + curDate.ToString("MMMM yyyy"));
+                curDate = curDate.AddMonths(1);
+            }
+            curDate = curDate.AddMonths(-1);
+            sMonthBox.Text = curDate.ToString("MMMM yyyy");
+        }
+
+
         private void print_reports_options_Shown(object sender, EventArgs e)
         {
             /* Set Default Start Date to One Month Ago */
-            input_startdate.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month - 1, 1);
+            //input_startdate.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month - 1, 1);
+
+            // Set Date Range Boxes
+            input_startdate.Value = sDateBox.Value;
+            input_enddate.Value = sDateBox.Value;
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Close();
+            button1.Enabled = false; // Stop accidental double clicking 2011-08-08
             Application.DoEvents();
 
             /* OK Clicked, Do Your Magic :) */
+            input_startdate.Value = input_startdate.Value.Date; // Set to Midnight 2011-08-08
+            
+            TimeSpan input_endtime = new TimeSpan(23, 59, 59);
+            input_enddate.Value = input_enddate.Value.Date + input_endtime; // Set to 11:59:59 2011-08-08
+
             double start_unixtime = mysqlglobal.ConvertToUnixTimestamp(input_startdate.Value);
-            double end_unixtime   = mysqlglobal.ConvertToUnixTimestamp(input_enddate.Value);
+            double end_unixtime   = mysqlglobal.ConvertToUnixTimestamp(input_enddate.Value); // Since it's to 00:00 UTC
+
+            /*
+            Clipboard.SetText(start_unixtime.ToString());
+            MessageBox.Show(@"Set Clipboard to Start Unixtime: " + start_unixtime);
+
+            Clipboard.SetText(end_unixtime.ToString());
+            MessageBox.Show(@"Set Clipboard to End Unixtime: " + end_unixtime);
+            */
+
             string output = "printer";
             if (radioButtonScreen.Checked == true) output = "screen";
 
-            m_parent.print_report_method(report_name, start_unixtime, end_unixtime, output);
-            
+            string totals_shown = "";
+            if (total_daily.Checked)
+            {
+                totals_shown = ", Daily";
+            }
+            if (total_monthly.Checked)
+            {
+                totals_shown += ", Monthly";
+            }
+            if (total_grand.Checked)
+            {
+                totals_shown += ", Grand";
+            }
+
+            totals_shown = totals_shown.Substring(2) + ".";
+
+            string report_type = "";
+            if (report_detailed.Checked) report_type = "Detailed.";
+            if (report_summary.Checked) report_type = "Summary.";
+
+            string consignor_code = consignorBox.Text;
+            double upto_unixtime = mysqlglobal.ConvertToUnixTimestamp(expiryUpto.Value);
+
+
+            m_parent.print_report_method(report_name, start_unixtime, end_unixtime, output, totals_shown, report_type, consignor_code, upto_unixtime);
+            this.Close();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -47,9 +138,58 @@ namespace Multi_Express_Consignment
             this.Close();
         }
 
-        private void print_reports_options_Load(object sender, EventArgs e)
+        private void sMonthBox_Click(object sender, EventArgs e)
+        {
+            sDateCheck.Checked = false;
+            sMonthCheck.Checked = true;
+            sMonthBox_SelectedValueChanged(null, null);
+            total_monthly.Checked = true;
+        }
+
+        private void sDateCheck_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void sDateBox_ValueChanged(object sender, EventArgs e)
+        {
+
+            sDateCheck.Checked = true;
+            sMonthCheck.Checked = false;
+            total_monthly.Checked = false;
+            // Set Date Range Boxes
+            input_startdate.Value = sDateBox.Value;
+            input_enddate.Value = sDateBox.Value;
+        }
+
+        private void sMonthBox_VisibleChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void sMonthBox_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+
+            // Convert Selected Date into DateTime
+            DateTime monthStart = DateTime.Parse(sMonthBox.Text);
+            DateTime monthEnd = monthStart.AddMonths(1);
+            monthEnd = monthEnd.AddDays(-1);
+            // Set Date Range Boxes
+            input_startdate.Value = monthStart;
+            input_enddate.Value = monthEnd;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            select_vendor_or_customer select_vendor_var = new select_vendor_or_customer("vendor",true);
+            select_vendor_var.ShowDialog();
+            string consignor = select_vendor_var.return_var;
+            consignorBox.Text = consignor;
+
+        }
+
+
     }
 }
