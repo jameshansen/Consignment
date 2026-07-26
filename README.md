188beab26a379b50ff18b5be98df1bf5b38f4312
James Hansen <1550976+jameshansen@users.noreply.github.com>
2026-07-26 01:49:39 -0700

    Public release

---
+++ README.md
@@ -0,0 +1,62 @@
+# Consignment Manager
+
+C# WinForms Application. Management program for retail stores that work on a consignment system (% shared on sale).
+
+**Background**
+
+I wrote this Consignment Management program for a Consignment Store located in Vancouver in 2011. I was employed by CompuMAX to develop this system. The CEO, Ken Tung, gave me permission to share the source code of the project publicly on my GitHub under a GPL license. Thank you Ken.
+
+This project was written The project took 6-12 months to complete from start to the first version beginning in January 2011, the first release to the customer in August 2011, followed by updates through to December 2011.
+
+**Concept**
+
+The concept is simple, a vendor provides items to a store to stock on their shelves. Upon the sale, the store pays the vendor a percentage of the revenue.
+
+The program provides inventory and vendor management, consignment intake and sales, consignor payout tracking, and reporting.
+
+**Capabilities**
+
+- **Vendor and customer records** with lookup by customer code, first/last name, or phone.
+- **Item-level inventory keyed to UPC/barcodes**, including printable **barcode item labels**.
+- **An order status workflow**: open, pending, in progress, invoiced, work completed, cancelled.
+- **Payment and payout tracking** to consignors (a payments ledger, net-payment-after-commission calculations, a cash log).
+- **A full Crystal Reports suite**: consignment agreement (the contract with the consignor), consignment invoice, sale receipt, daily sales, order detail, net/consolidated consignor payment reports, and a cash log report.
+- **Data import/export** for moving the dataset in and out.
+
+**Framework**
+
+The framework is a little unconventional. While it is a Windows desktop application written in C#, MySQL was used for the database, rather than MSSQL, due to my familiarity at the time after working with MySQL in PHP.
+
+**Requirements**
+
+Crystal Reports Runtime is required which includes the required CrystalDecisions.* References - install the appropriate "CRRuntime" msi file for your system.
+
+MySQL Connector .net is required - the original connector 6.3.4 installer is included here - mysql.data.msi
+
+The database structure has to be prepopulated from consignment_db_structure.sql
+
+**Important Notes**
+
+This code is old, and is shared here for historical and informational purposes.
+
+It's requirements and dependancies are old, which likely means the code is insecure and likely contains a number of CVEs. These will be addressed in due time.
+
+Additionally the project is hard-coded for one store and still uses "HST" instead of the "GST/PST" combination for sales taxes as this was in place in BC during 2011 and was dropped in 2013. This will also be addressed in a future update.
+
+Finally there are a number of plain-text MySQL queries in the source code which need to be replaced with LINQ or another safer method.
+
+**Release**
+
+Originally an installer was used that setup the database and installed all dependancies - this is not included in the Git repository.
+
+There will not be a public binary at this time release until the other issues are addressed and I confirm the code is safe to distribute.
+
+**Screenshots**
+
+![Consignment Manager screenshot 1](screens/consignment_01.png)
+
+![Consignment Manager screenshot 2](screens/consignment_02.png)
+
+![Consignment Manager screenshot 3](screens/consignment_03.png)
+
+![Consignment Manager screenshot 4](screens/consignment_04.png)
---
+++ consignment_db_structure.sql
@@ -0,0 +1,297 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 02, 2011 at 01:56 PM
+-- Server version: 5.1.37
+-- PHP Version: 5.2.10-2ubuntu6.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `consignment_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `CSTITEM`
+--
+
+CREATE DATABASE consignment_db;
+USE consignment_db;
+
+CREATE TABLE IF NOT EXISTS `CSTITEM` (
+  `upc` int(11) NOT NULL AUTO_INCREMENT,
+  `consignment_code` varchar(20) NOT NULL,
+  `order_number` int(11) NOT NULL,
+  `vendor_code` varchar(20) NOT NULL,
+  `customer_code` varchar(20) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `price_minimum` decimal(16,4) NOT NULL,
+  `price_suggested` decimal(16,4) NOT NULL,
+  `price_sale` decimal(16,4) NOT NULL,
+  `share` decimal(16,4) NOT NULL,
+  `share_type` enum('value','percentage') NOT NULL,
+  `status` enum('unsold','sold') NOT NULL,
+  `consignment_status` text NOT NULL,
+  `date_received` bigint(20) NOT NULL,
+  `date_expiry` bigint(20) NOT NULL,
+  `date_sold` bigint(20) NOT NULL,
+  `date_paid` bigint(20) NOT NULL,
+  `desc_brand` text NOT NULL,
+  `desc_gender` text NOT NULL,
+  `desc_garment` text NOT NULL,
+  `desc_material` text NOT NULL,
+  `desc_colour` text NOT NULL,
+  `desc_size` text NOT NULL,
+  PRIMARY KEY (`upc`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `CSTORDER`
+--
+
+CREATE TABLE IF NOT EXISTS `CSTORDER` (
+  `order_number` int(11) NOT NULL AUTO_INCREMENT,
+  `invoice_number` text NOT NULL,
+  `status` text NOT NULL,
+  `order_status` text NOT NULL,
+  `customer_code` varchar(20) NOT NULL,
+  `customer_first_name` text NOT NULL,
+  `customer_last_name` text NOT NULL,
+  `date_order` bigint(20) NOT NULL,
+  `items` int(11) NOT NULL,
+  `total` decimal(16,4) NOT NULL,
+  PRIMARY KEY (`order_number`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `CSTPAYMENT`
+--
+
+CREATE TABLE IF NOT EXISTS `CSTPAYMENT` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `consignment_code` varchar(20) DEFAULT NULL,
+  `order_number` varchar(20) DEFAULT NULL,
+  `type` text NOT NULL,
+  `description` text NOT NULL,
+  `cn` text NOT NULL,
+  `expiry` text NOT NULL,
+  `date` bigint(20) NOT NULL,
+  `amount` decimal(16,4) NOT NULL,
+  `vendor_code` text NOT NULL,
+  `vendor_name` text NOT NULL,
+  `customer_code` text NOT NULL,
+  `customer_name` text NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `PSVEMAST`
+--
+
+CREATE TABLE IF NOT EXISTS `PSVEMAST` (
+  `CMCUCODE` text,
+  `CMCUNAME` text,
+  `CMADD1` text,
+  `CMADD2` text,
+  `CMPOST` text,
+  `CMPHONE` text,
+  `CMFAX1` text,
+  `CMORDERAMT` decimal(15,2) DEFAULT NULL,
+  `CMAVGPAY` decimal(15,1) DEFAULT NULL,
+  `CMCREDLIMT` int(11) DEFAULT NULL,
+  `CMYTDSALES` decimal(15,2) DEFAULT NULL,
+  `CMYTDINCNT` int(11) DEFAULT NULL,
+  `CMLYRSALE` decimal(15,2) DEFAULT NULL,
+  `CMLYRINCNT` int(11) DEFAULT NULL,
+  `CMSTATSW` text,
+  `CMINTERSW` text,
+  `CMBALANCE` decimal(15,2) DEFAULT NULL,
+  `CMACCODE` text,
+  `CMDEPT` text,
+  `CMDISCOUNT` decimal(15,2) DEFAULT NULL,
+  `CMDISCDAY` int(11) DEFAULT NULL,
+  `CMNETDAY` int(11) DEFAULT NULL,
+  `CMTITLE` text,
+  `CMNAME1ST` text,
+  `CMNAMESUR` text,
+  `CMTELEXT1` text,
+  `CMCITY` text,
+  `CMSTATE` text,
+  `CMCOUNTRY` text,
+  `CMMEMO1` text,
+  `CMFOB` text,
+  `CMVIA` text,
+  `CMSALESMAN` text,
+  `CMTAXNUM1` text,
+  `CMTAXNUM2` text,
+  `CMTERMDESC` text,
+  `CMGLGROUP` text,
+  `CMINVCNT` int(11) DEFAULT NULL,
+  `CMPAYDAY` int(11) DEFAULT NULL,
+  `CMNYRSALE` decimal(15,2) DEFAULT NULL,
+  `CMNYRINCNT` int(11) DEFAULT NULL,
+  `CMHOLDSW` text,
+  `CMT1DESC` text,
+  `CMT2DESC` text,
+  `CMDISCTYPE` text,
+  `CMSTARTDAT` text,
+  `CMRENEWDAT` text,
+  `CMPAYDAT` text,
+  `CMINVDAT` text,
+  `CMACTDAT` text,
+  `CMMODDAT` text,
+  `CMPHONE2` text,
+  `CMTELEXT2` text,
+  `CMTERR` text,
+  `CMBUY1ST` text,
+  `CMBUYLAST` text,
+  `CMCUGROUP` text,
+  `CMRPGROUP` text,
+  `CMEMAIL` text,
+  `CMPHONE3` text,
+  `CMPHONE4` text,
+  `CMTELEXT3` text,
+  `CMTELEXT4` text,
+  `CMTELDESC2` text,
+  `CMTELDESC3` text,
+  `CMTELDESC4` text,
+  `CMDESRENEW` text,
+  `CMDESDATE1` text,
+  `CMDESDATE2` text,
+  `CMDATE1` text,
+  `CMDATE2` text,
+  `CMBITMAP` text,
+  `CMHANDLER` text,
+  `CMPREFERHN` text,
+  `CMRECALL` text,
+  `CMRECDATE` text,
+  `CMRECREM` text,
+  `CMCURR` text,
+  `CMFOLDER` text,
+  `CMLYGROUP` text,
+  `CMOTAXAPP` text,
+  `CMOTAXCODE` text,
+  `CMDISCRATE` decimal(15,2) DEFAULT NULL,
+  `CMARPURGE` text,
+  `CMLOCATION` text
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SFCUMAST`
+--
+
+CREATE TABLE IF NOT EXISTS `SFCUMAST` (
+  `CMCUCODE` text,
+  `CMCUNAME` text,
+  `CMADD1` text,
+  `CMADD2` text,
+  `CMPOST` text,
+  `CMPHONE` text,
+  `CMFAX1` text,
+  `CMORDERAMT` decimal(15,2) DEFAULT NULL,
+  `CMAVGPAY` decimal(15,1) DEFAULT NULL,
+  `CMCREDLIMT` int(11) DEFAULT NULL,
+  `CMYTDSALES` decimal(15,2) DEFAULT NULL,
+  `CMYTDINCNT` int(11) DEFAULT NULL,
+  `CMLYRSALE` decimal(15,2) DEFAULT NULL,
+  `CMLYRINCNT` int(11) DEFAULT NULL,
+  `CMSTATSW` text,
+  `CMINTERSW` text,
+  `CMBALANCE` decimal(15,2) DEFAULT NULL,
+  `CMACCODE` text,
+  `CMDEPT` text,
+  `CMDISCOUNT` decimal(15,2) DEFAULT NULL,
+  `CMDISCDAY` int(11) DEFAULT NULL,
+  `CMNETDAY` int(11) DEFAULT NULL,
+  `CMTITLE` text,
+  `CMNAME1ST` text,
+  `CMNAMESUR` text,
+  `CMTELEXT1` text,
+  `CMCITY` text,
+  `CMSTATE` text,
+  `CMCOUNTRY` text,
+  `CMMEMO1` text,
+  `CMFOB` text,
+  `CMVIA` text,
+  `CMSALESMAN` text,
+  `CMTAXNUM1` text,
+  `CMTAXNUM2` text,
+  `CMTERMDESC` text,
+  `CMGLGROUP` text,
+  `CMINVCNT` int(11) DEFAULT NULL,
+  `CMPAYDAY` int(11) DEFAULT NULL,
+  `CMNYRSALE` decimal(15,2) DEFAULT NULL,
+  `CMNYRINCNT` int(11) DEFAULT NULL,
+  `CMHOLDSW` text,
+  `CMT1DESC` text,
+  `CMT2DESC` text,
+  `CMDISCTYPE` text,
+  `CMSTARTDAT` text,
+  `CMRENEWDAT` text,
+  `CMPAYDAT` text,
+  `CMINVDAT` text,
+  `CMACTDAT` text,
+  `CMMODDAT` text,
+  `CMPHONE2` text,
+  `CMTELEXT2` text,
+  `CMTERR` text,
+  `CMBUY1ST` text,
+  `CMBUYLAST` text,
+  `CMCUGROUP` text,
+  `CMRPGROUP` text,
+  `CMEMAIL` text,
+  `CMPHONE3` text,
+  `CMPHONE4` text,
+  `CMTELEXT3` text,
+  `CMTELEXT4` text,
+  `CMTELDESC2` text,
+  `CMTELDESC3` text,
+  `CMTELDESC4` text,
+  `CMDESRENEW` text,
+  `CMDESDATE1` text,
+  `CMDESDATE2` text,
+  `CMDATE1` text,
+  `CMDATE2` text,
+  `CMBITMAP` text,
+  `CMHANDLER` text,
+  `CMPREFERHN` text,
+  `CMRECALL` text,
+  `CMRECDATE` text,
+  `CMRECREM` text,
+  `CMCURR` text,
+  `CMFOLDER` text,
+  `CMOTAXAPP` text,
+  `CMOTAXCODE` text,
+  `CMCUSFIELD` text,
+  `CMDISCRATE` decimal(15,2) DEFAULT NULL,
+  `CMARPURGE` text,
+  `CMLOCATION` text,
+  `CMSHIPTO` text,
+  `CMALSHIPTO` text,
+  `CMMTAXCODE` text,
+  `CMMTAXAPP` text
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
